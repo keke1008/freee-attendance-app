a818701dc2c261809c4350366ebf1db8add0f650
keke1008 <keke2222337@gmail.com>
2023-03-21 16:14:49 +0900

    refactor: move pagination into concern

--- app/controllers/managers_controller.rb
+++ app/controllers/managers_controller.rb
@@ -1,56 +1,15 @@
-# メソッドの引数のspanには，'day', 'week', 'month'のうちどれかが入る
 class ManagersController < ApplicationController
+  include SpanPagination
+
   before_action :set_manager
+  before_action :set_pagination
 
   def show
-    @date = date_from_params
-    @span = span_from_params
-
-    @date_range = aggrigate_date_range(@date, @span)
-    @current_date_string = current_date_string(@date, @date_range, @span)
-    @previous_date, @next_date = previous_and_next_date(@date, @span)
-
     @employees = @manager.employee
     @employees_attendances = aggrigate_total_attendance_minute(@date_range)
   end
 
   private
-
-  # paramsの:dateを参照し，Dateを返す
-  # デフォルトは今日の日付
-  def date_from_params
-    params[:date]&.to_date || Date.current
-  end
-
-  # paramsの:spanを参照し，'day', 'week', 'month'のうちどれかを返す
-  # デフォルトは'day'
-  def span_from_params
-    span = params[:span]
-    %w[day week month].include?(span) ? span : 'day'
-  end
-
-  # 集計するデータの範囲を返す
-  def aggrigate_date_range(date, span)
-    date.send("all_#{span}")
-  end
-
-  # 集計範囲の前の日付と次の日付を計算する
-  def previous_and_next_date(date, span)
-    offset = 1.send(span.to_s)
-    [date.ago(offset), date.since(offset)]
-  end
-
-  # 「現在表示している日付の範囲」を表す文字列を返す
-  def current_date_string(date, date_range, span)
-    case span
-    when 'day'
-      I18n.l(date)
-    when 'week'
-      "#{I18n.l(date_range.first)} ~ #{I18n.l(date_range.last)}"
-    else
-      I18n.l(date, format: :year_month)
-    end
-  end
 
   # { 従業員名 => block(従業員の出勤記録の配列) } を返す
   def aggrigate_employees_attendances(date_range)
