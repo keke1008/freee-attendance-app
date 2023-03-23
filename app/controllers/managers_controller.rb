bcad871cca6f8abb8d56a6414ecfa127ce25e975
keke1008 <keke2222337@gmail.com>
2023-03-24 02:45:09 +0900

    fix: employee visiblity bug

--- app/controllers/managers_controller.rb
+++ app/controllers/managers_controller.rb
@@ -15,10 +15,11 @@ class ManagersController < ApplicationController
   private
 
   def to_graph_data(model)
-    model.where(date: @page.date_range)
-         .collect_duration_sec
-         .map do |employee, duration_sec|
-      duration_hours = duration_sec.fdiv(3600).ceil(1)
+    model.where(
+      date: @page.date_range,
+      employee: @manager.employee
+    ).collect_duration_sec.map do |employee, duration_sec|
+      duration_hours = duration_sec.fdiv(3600).ceil(1) # 秒 => 時間
       [employee.name, duration_hours]
     end
   end
